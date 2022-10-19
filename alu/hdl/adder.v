e57c07fe1736d1fdbe47f719b2d6b131049dd80d
amruth-sn <106125855+amruth-sn@users.noreply.github.com>
2022-10-19 19:24:17 -0400

    Create adder.v

---
+++ alu/hdl/adder.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+// Amruth Niranjan
+// October 2022
+
+module adder(
+    input [3:0] A,
+    input [3:0] B,
+    output [7:0] sum
+    );
+    assign sum = A + B;
+endmodule
